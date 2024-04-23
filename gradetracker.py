b413924687f87c45a0f6a856087e43abaa05f245
oswaldamoah <oswaldamoah@gmail.com>
2024-04-23 23:44:49 +0000

    updated grade tracker

--- gradetracker.py
+++ gradetracker.py
@@ -1,91 +1,206 @@
-# Making text input required with a function
-def get_course_code():
-   while True:
-       course_code = str(input("Enter Course code: "))
-       if course_code.strip():  # Check if course code is not empty
-           return course_code
-       else:
-           print("Course code is required. Please enter a valid code.")
+class Course:
+    def __init__(self, name, final_score, grade, remark):
+        self.name = name
+        self.final_score = final_score
+        self.grade = grade
+        self.remark = remark
 
-# Making numerical input required with a function
-def get_float_input(prompt):
-   while True:
-       try:
-           return float(input(prompt))
-       except ValueError:
-           print("Invalid input. Please enter a number.")
+def calculate_grade(final_score):
+    if final_score >= 85:
+        final_grade = "A+"
+    elif final_score >= 80:
+        final_grade = "A"
+    elif final_score >= 75:
+        final_grade = "B+"
+    elif final_score >= 70:
+        final_grade = "B"
+    elif final_score >= 65:
+        final_grade = "C+"
+    elif final_score >= 60:
+        final_grade = "C"
+    elif final_score >= 55:
+        final_grade = "D+"
+    elif final_score >= 50:
+        final_grade = "D"
+    else:
+        final_grade = "E"
+    
+    return final_grade
 
-# Input course code
-course_code = get_course_code()  
-
-# Input Assignment Scores
-total_assignment_score = get_float_input("Enter Assignment 1 Score (20): ") + get_float_input("Enter Assignment 2 Score (30) : ")
-
-# Input Exam Score
-exam_score = float(input("Enter Exam Score (100): ")) / 100
-
-# Expression for Average Assignment
-assignment_average = total_assignment_score / 50
-
-
-# Expression for final Score
-final_score = (assignment_average * 30) + (exam_score * 70)
-
-
-# Conditional Statement for finding grade
-if final_score >= 85:
-    final_grade = "A+"
-elif final_score >= 80:
-    final_grade = "A"
-elif final_score >= 75:
-    final_grade = "B+"
-elif final_score >= 70:
-    final_grade = "B"
-elif final_score >= 65:
-    final_grade = "C+"
-elif final_score >= 60:
-    final_grade = "C"
-elif final_score >= 55:
-    final_grade = "D+"
-elif final_score >= 50:
-    final_grade = "D"
-else:
-    final_grade = "E"
-
-# Expression for GPA
-gpa = (final_score / 100) * 4
+def calculate_remark(final_score):
+    if final_score >= 85:
+        remark = "Outstanding"
+    elif final_score >= 80:
+        remark = "Excellent"
+    elif final_score >= 75:
+        remark = "Very Good"
+    elif final_score >= 70:
+        remark = "Good"
+    elif final_score >= 65:
+        remark = "Average"
+    elif final_score >= 60:
+        remark = "Pass"
+    elif final_score >= 55:
+        remark = "Poor"
+    elif final_score >= 50:
+        remark = "Very Poor"
+    else:
+        remark = "Fail"
+    
+    return remark
 
 
-# Display results
-print(f"\nCOURSE: {course_code}\n"
-     f"RAW SCORE: {final_score:.2f}/100\n"
-     f"GRADE: {final_grade}\n"
-     f"GPA: {gpa:.2f}")
+# Courses
+def math():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+ 
+    return Course("Mathematics", 0, grade, remark)
+
+def int_science():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+
+    return Course("Integrated Science", 0, grade, remark)
+
+def social():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+    
+    return Course("Social Studies", 0, grade, remark)
+
+def english():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+    
+    return Course("English Language", 0, grade, remark)
+
+def bio():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+    
+    return Course("Biology", 0, grade, remark)
+
+def chem():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+
+    return Course("Chemistry", 0, grade, remark)
+
+def phys():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+
+    return Course("Physics", final_score, grade, remark)
+
+def ict():
+    final_score = 0
+    grade = calculate_grade(final_score)
+    remark = calculate_remark(final_score)
+
+    return Course("Information Technology", final_score, grade, remark)
+
+math = math()
+int_science = int_science()
+social = social()
+english = english()
+bio = bio()
+chem = chem()
+phys = phys()
+ict = ict()
 
 
-# DONE
-"""
-* Take scores input for one course
-* Calculate and display Raw Score and GPA
-"""
-
-#TO DO
-"""
-* Add remark system to if statement and call it in printed results 
-* Make code interactive:
-Welcome to LMS for UG
-    - Enter scores (Adds new scores or overwrites all old scores)
-        What course would you like to add scores for?
-        - All 7 courses listed (depending on what is chosen, it is noted) # U can use dictionary of ussd style {"1":"MATH223" and so on}
-          # Ask same previous input questions, but for that specific object course (assignment1, assignment2, examscore)
-          # Calculate and Display grades for all courses based on if statement
-          # Calculate and Display collective RAW Score and GPA 
-    # Draw a class diagram for the OOP
-    # Complete ReadMe.md
+courses = [math, int_science, social, english, bio, chem, phys, ict]
 
 
-You can select courses from a list of 7 courses (Each course will be an object)
-All course raw scores will be added together to get the gpa
-Function to edit course info
 
-"""
+def start():
+     print("OT High School Grade Tracker")
+     print(f"\t1. Enter Scores\n\t2. Get Course Information\n\t3. Edit Scores\n\t4. Reset Course\n\t5. General Information\n\tx. Exit")
+
+     while True:
+        choice1 = input("* Select an option: ")
+        print("")
+
+
+        match choice1:
+            case '1':
+                enter_scores()
+                goHome()
+                break
+            case '2':
+                course_info()
+                goHome()
+                break
+            case '3':
+                enter_scores()
+                goHome()
+                break
+            case '4':
+                reset_scores()
+                goHome()
+                break
+            case '5':
+                general_info()
+                goHome()
+                break
+
+
+def enter_scores():
+     ''' REMOVE COMMENT AND EDIT
+     print("Which course score would you like to enter?")
+     print(f"\t1. Math\n\t2. Science\n\t3. Edit Scores\n\t4. Reset Course\n\t5. General Information\n\tx. Exit")
+     print(f"\t1. Enter Scores\n\t2. Get Course Information\n\t3. Edit Scores\n\t4. Reset Course\n\t5. General Information\n\tx. Exit")
+
+     while True:
+        choice1 = input("* Select an option: ")
+        print("")
+
+
+        match choice1:
+            case '1':
+                enter_scores()
+                goHome()
+                break
+            case '2':
+                course_info()
+                goHome()
+                break
+            case '3':
+                enter_scores()
+                goHome()
+                break
+            case '4':
+                reset_scores()
+                goHome()
+                break
+            case '5':
+                general_info()
+                goHome()
+                break
+                
+                '''
+
+
+def goHome():
+    print("")
+    
+    while True:
+        decision = input("Homepage-'0' | Exit-'x' : ")
+        print("")
+
+        match decision:
+            case '0':
+                start()
+                break
+            case 'x':
+                break
+            case _:
+                print("Enter either '0 - Home' or 'x - Exit'")
